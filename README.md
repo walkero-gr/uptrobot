8024dbd9ce4389c2970ca336ac9748b6dc6610f0
George Sokianos <walkero@gmail.com>
2018-08-23 18:27:27 +0300

    - Added the follow mode with autorefreshing of the monitor list

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 Features:
 - show the monitors the user has created and their status
+- use follow mode to autorefresh the list in every n secs (default: 300). The update seconds can be set using the **-s SLEEP, --sleep SLEEP** parameter.
 
 
 ### Usage
-Before you use the script you have to edit it and add the your API key. The UpTime Robot API key can be found under "My Settings" page.
+Before you use the script you have to edit it and add your API key. The UpTime Robot API key can be found under "My Settings" page.
 
 The above values should be entered at the following line of the script, by replacing the dummy values.
 
@@ -14,7 +15,7 @@ key = "YOUR_API_KEY"
 ```
 As soon as you save the script you are good to go. Open a terminal and run the script using 
 ```bash
-uptrobot.py [-h] [--version]
+uptrobot.py [-h] [-f] [-s SLEEP] [--version]
 ```
 
 To make it available from any folder on your Linux machine, you can edit the .bashrc file under your user home folder and add the following line at the end of the file.
@@ -25,13 +26,18 @@ export PATH=$PATH:</path/to/file>
 
 #### Parameters and Actions
 ```bash
-uptrobot.py [-h] [--version]
+uptrobot.py [-h] [-f] [-s SLEEP] [--version]
 
 This is a python script that can be used to get information about your
 monitors at UpTime Robot (https://uptimerobot.com). For more information visit
 https://github.com/walkero-gr/uptrobot
 
 optional arguments:
-  -h, --help  show this help message and exit
-  --version   show program's version number and exit
+  -h, --help            show this help message and exit
+  -f, --follow          keep requesting information until the user press
+                        CTRL-C
+  -s SLEEP, --sleep SLEEP
+                        set the maximum sleep time in seconds, when using the
+                        follow mode
+  --version             show program's version number and exit
 ```
--- uptrobot.py
+++ uptrobot.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 
-import urllib3, sys, argparse, json, datetime
+import urllib3, sys, argparse, json, datetime, time, curses, signal
 # from pprint import pprint
 
 http = urllib3.PoolManager()
 
-ver = "0.1"
+ver = "0.2"
 
 # The UpTime Robot API key can be found under "My Settings" page
 key = "YOUR_API_KEY"
 
+defSleepTime = 300
 
 class color:
    PURPLE = '\033[95m'
@@ -48,10 +49,14 @@ statuses = {
 }
 
 
-
 def introText():
 	print 'uptrobot.py v' + ver + ' - Created by George Sokianos'
 
+
+def signal_handler(sig, frame):
+    curses.endwin()
+    print('You pressed Ctrl+C!')
+    sys.exit(0)
 
 
 def apiGetMonitors():
@@ -68,37 +73,93 @@ def apiGetMonitors():
 
     response = request.status
     retData = json.loads(request.data)
-    # print response
-    # print request.data
-    return retData
+
+    if response == 200 and retData['stat'] == 'ok' :
+        return retData
+    else :
+        return 0
 
 
 def printMonitors(rawdata):
-    print "Found " + str(rawdata["pagination"]['total']) + " monitors\n"
-    print "ID              Name                    Status"
-    print "----------      --------------------    ----------"
+    nowStr = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+    sys.stdout.write("Last update: {0}\n\r".format(nowStr))
+    sys.stdout.write("Found " + str(rawdata["pagination"]['total']) + " monitors\n\n\r")
+    sys.stdout.write("ID              Name                    Status\n\r")
+    sys.stdout.write("----------      --------------------    ----------\n\r")
     for monitor in rawdata["monitors"] :
         statusId = str(monitor['status'])               # Get the monitor status ID
         statusStr = statuses[statusId]['descr']         # Get the description for this status, from the dictionary
         display = statuses[statusId]['color']           # Get the color for this status, from the dictionary
         statusChangeTime = monitor['logs'][0]['datetime']       # Get the last status change datetime, which is a timestamp
         datetimeStr = datetime.datetime.fromtimestamp(statusChangeTime).strftime('%Y-%m-%d %H:%M')      # Convert the last status change datetime to human datetime
-        print "{0:<10}\t{1:<20}\t{colorStr}{2:} since {3} {endStr}".format(monitor['id'], monitor['friendly_name'], statusStr, datetimeStr, colorStr=display,endStr=color.END)
+        sys.stdout.write("{0:<10}\t{1:<20}\t{colorStr}{2:} since {3} {endStr}\n\r".format(monitor['id'], monitor['friendly_name'], statusStr, datetimeStr, colorStr=display,endStr=color.END))
 
 
 def main(argv):
+    action = ''
+    sleepTime = defSleepTime
 
     # Parse the arguments
     argParser = argparse.ArgumentParser(description='This is a python script that can be used to get information about your monitors at UpTime Robot (https://uptimerobot.com). For more information visit https://github.com/walkero-gr/uptrobot ')
+    argParser.add_argument('-f', '--follow', action='store_true', default=False, dest='follow', help='keep requesting information until the user press CTRL-C')
+    argParser.add_argument('-s', '--sleep', action='store', dest='sleep', type=int, help='set the maximum sleep time in seconds, when using the follow mode')
 
     argParser.add_argument('--version', action='version', version='%(prog)s v' + ver)
 
     args = argParser.parse_args()
+    if args.follow :
+        action = 'follow'
+    if args.sleep :
+        sleepTime = args.sleep
 
     introText()
 
-    monitorsData = apiGetMonitors()
-    printMonitors(monitorsData)
+    if action == 'follow':
+        signal.signal(signal.SIGINT, signal_handler)
+
+        stdscr = curses.initscr()
+        curses.curs_set(0)
+        stdscr.clear()
+        stdscr.refresh()
+
+        sys.stdout.write('Enabled follow mode. Press Ctrl+C to exit. \n\r')
+        monitorsData = apiGetMonitors()
+        if monitorsData :
+            printMonitors(monitorsData)
+        else :
+            curses.endwin()
+            print "There was an error with the API request. Type -h to see the help text."
+            sys.exit(2)
+
+        timeLeft = sleepTime
+        while True:
+            if timeLeft == 0 :
+                monitorsData = apiGetMonitors()
+                if monitorsData :
+                    stdscr.clear()
+                    stdscr.refresh()
+                    # curses.flash()
+
+                    sys.stdout.write('Enabled follow mode. Press Ctrl+C to exit. \n\r')
+                    printMonitors(monitorsData)
+                else :
+                    curses.endwin()
+                    print "There was an error with the API request. Type -h to see the help text."
+                    sys.exit(2)
+
+                timeLeft = sleepTime
+            else :
+                sys.stdout.write('Update in {0} secs \r'.format(timeLeft))
+                sys.stdout.flush()
+                timeLeft = timeLeft - 1
+            time.sleep(1)
+    else :
+        monitorsData = apiGetMonitors()
+        if monitorsData :
+            printMonitors(monitorsData)
+        else :
+            print "There was an error with the API request. Type -h to see the help text."
+            sys.exit(2)
 
 
 
