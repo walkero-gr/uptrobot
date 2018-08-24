cb4153952f7d82514bee09ebbe313077fb9fe572
George Sokianos <walkero@gmail.com>
2018-08-24 11:09:05 +0300

    - Introduced msg dictionary, where all the messages are assigned to specific code numbers - Introduced APIHandler class with all the methods to make the calls from the API - Introduced outputHandler class with all the methods to print data to the terminal - Cleaned up the code a little bit and added more comments

--- uptrobot.py
+++ uptrobot.py
@@ -5,7 +5,7 @@ import urllib3, sys, argparse, json, datetime, time, curses, signal
 
 http = urllib3.PoolManager()
 
-ver = "0.2"
+ver = "0.3"
 
 # The UpTime Robot API key can be found under "My Settings" page
 key = "YOUR_API_KEY"
@@ -13,39 +13,39 @@ key = "YOUR_API_KEY"
 defSleepTime = 300
 
 class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
+   PURPLE       = '\033[95m'
+   CYAN         = '\033[96m'
+   DARKCYAN     = '\033[36m'
+   BLUE         = '\033[94m'
+   GREEN        = '\033[92m'
+   YELLOW       = '\033[93m'
+   RED          = '\033[91m'
+   BOLD         = '\033[1m'
+   UNDERLINE    = '\033[4m'
+   END          = '\033[0m'
 
 
+# Monitors' status decoding
 statuses = {
-    '0': {
-        'descr': 'Paused',
-        'color': color.YELLOW
-    },
-    '1': {
-        'descr': 'Not checked yet',
-        'color': color.YELLOW
-    },
-    '2': {
-        'descr': 'Up',
-        'color': color.GREEN
-    },
-    '8': {
-        'descr': 'Seems down',
-        'color': color.RED
-    },
-    '9': {
-        'descr': 'Down',
-        'color': color.RED
-    }
+    '0': { 'descr': 'Paused',           'color': color.YELLOW   },
+    '1': { 'descr': 'Not checked yet',  'color': color.YELLOW   },
+    '2': { 'descr': 'Up',               'color': color.GREEN    },
+    '8': { 'descr': 'Seems down',       'color': color.RED      },
+    '9': { 'descr': 'Down',             'color': color.RED      }
+}
+
+# Output messages
+msg = {
+    # 1xx Informational response
+    '101': 'Enabled follow mode. Press Ctrl+C to exit.',
+
+    # 2xx Success
+
+    # 3xx Client errors
+    '301': 'There was an error with the API request. Type -h to see the help text.',
+
+    # 4xx Server errors
+
 }
 
 
@@ -59,40 +59,54 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 
-def apiGetMonitors():
-    url = "https://api.uptimerobot.com/v2/{0}".format('getMonitors')
-    jsonFields = {
-        "api_key": key,
-        "format": "json",
-        "logs": 1
-    }
-    jsonHeaders = {
-        'cache-control': "no-cache"
-    }
-    request = http.request('POST', url, headers=jsonHeaders, fields=jsonFields, timeout=5.0)
 
-    response = request.status
-    retData = json.loads(request.data)
+class APIHandler:
+    def __init__(self):
+        self.baseurl = 'https://api.uptimerobot.com/v2/'
 
-    if response == 200 and retData['stat'] == 'ok' :
-        return retData
-    else :
-        return 0
+        return
 
 
-def printMonitors(rawdata):
-    nowStr = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-    sys.stdout.write("Last update: {0}\n\r".format(nowStr))
-    sys.stdout.write("Found " + str(rawdata["pagination"]['total']) + " monitors\n\n\r")
-    sys.stdout.write("ID              Name                    Status\n\r")
-    sys.stdout.write("----------      --------------------    ----------\n\r")
-    for monitor in rawdata["monitors"] :
-        statusId = str(monitor['status'])               # Get the monitor status ID
-        statusStr = statuses[statusId]['descr']         # Get the description for this status, from the dictionary
-        display = statuses[statusId]['color']           # Get the color for this status, from the dictionary
-        statusChangeTime = monitor['logs'][0]['datetime']       # Get the last status change datetime, which is a timestamp
-        datetimeStr = datetime.datetime.fromtimestamp(statusChangeTime).strftime('%Y-%m-%d %H:%M')      # Convert the last status change datetime to human datetime
-        sys.stdout.write("{0:<10}\t{1:<20}\t{colorStr}{2:} since {3} {endStr}\n\r".format(monitor['id'], monitor['friendly_name'], statusStr, datetimeStr, colorStr=display,endStr=color.END))
+    def getMonitors(self):
+        url = "{0}{1}".format(self.baseurl, 'getMonitors')
+        jsonFields = {
+            "api_key": key,
+            "format": "json",
+            "logs": 1
+        }
+        jsonHeaders = {
+            'cache-control': "no-cache"
+        }
+        request = http.request('POST', url, headers=jsonHeaders, fields=jsonFields, timeout=5.0)
+
+        response = request.status
+        retData = json.loads(request.data)
+
+        if response == 200 and retData['stat'] == 'ok' :
+            return retData
+        else :
+            return False
+
+
+class outputHandler:
+    def __init__(self):
+        self.rawdata = {}
+
+        return
+
+    def printMonitors(self):
+        nowStr = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+        sys.stdout.write("Last update: {0}\n\r".format(nowStr))
+        sys.stdout.write("Found " + str(self.rawdata["pagination"]['total']) + " monitors\n\n\r")
+        sys.stdout.write("ID              Name                    Status\n\r")
+        sys.stdout.write("----------      --------------------    ----------\n\r")
+        for monitor in self.rawdata["monitors"] :
+            statusId = str(monitor['status'])               # Get the monitor status ID
+            statusStr = statuses[statusId]['descr']         # Get the description for this status, from the dictionary
+            display = statuses[statusId]['color']           # Get the color for this status, from the dictionary
+            statusChangeTime = monitor['logs'][0]['datetime']       # Get the last status change datetime, which is a timestamp
+            datetimeStr = datetime.datetime.fromtimestamp(statusChangeTime).strftime('%Y-%m-%d %H:%M')      # Convert the last status change datetime to human datetime
+            sys.stdout.write("{0:<10}\t{1:<20}\t{colorStr}{2:} since {3} {endStr}\n\r".format(monitor['id'], monitor['friendly_name'], statusStr, datetimeStr, colorStr=display,endStr=color.END))
 
 
 def main(argv):
@@ -115,6 +129,9 @@ def main(argv):
     introText()
 
     if action == 'follow':
+        api = APIHandler()
+        output = outputHandler()
+
         signal.signal(signal.SIGINT, signal_handler)
 
         stdscr = curses.initscr()
@@ -122,29 +139,31 @@ def main(argv):
         stdscr.clear()
         stdscr.refresh()
 
-        sys.stdout.write('Enabled follow mode. Press Ctrl+C to exit. \n\r')
-        monitorsData = apiGetMonitors()
+        sys.stdout.write(msg['101'] + '\n\r')
+        monitorsData = api.getMonitors()
         if monitorsData :
-            printMonitors(monitorsData)
+            output.rawdata = monitorsData
+            output.printMonitors()
         else :
             curses.endwin()
-            print "There was an error with the API request. Type -h to see the help text."
+            print msg['301']
             sys.exit(2)
 
         timeLeft = sleepTime
         while True:
             if timeLeft == 0 :
-                monitorsData = apiGetMonitors()
+                monitorsData = api.getMonitors()
                 if monitorsData :
                     stdscr.clear()
                     stdscr.refresh()
                     # curses.flash()
 
-                    sys.stdout.write('Enabled follow mode. Press Ctrl+C to exit. \n\r')
-                    printMonitors(monitorsData)
+                    sys.stdout.write(msg['101'] + '\n\r')
+                    output.rawdata = monitorsData
+                    output.printMonitors()
                 else :
                     curses.endwin()
-                    print "There was an error with the API request. Type -h to see the help text."
+                    print msg['301']
                     sys.exit(2)
 
                 timeLeft = sleepTime
@@ -154,11 +173,14 @@ def main(argv):
                 timeLeft = timeLeft - 1
             time.sleep(1)
     else :
-        monitorsData = apiGetMonitors()
+        api = APIHandler()
+        monitorsData = api.getMonitors()
         if monitorsData :
-            printMonitors(monitorsData)
+            output = outputHandler()
+            output.rawdata = monitorsData
+            output.printMonitors()
         else :
-            print "There was an error with the API request. Type -h to see the help text."
+            print msg['301']
             sys.exit(2)
 
 
